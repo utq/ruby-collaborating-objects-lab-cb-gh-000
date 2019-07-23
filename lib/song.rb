2e26be17e8d770fe0ae91f9fd31860110beecb8a
Celal Cankaya <celalcankaya@outlook.com>
2019-07-23 15:16:33 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require_relative "../lib/artist.rb"
+require_relative "../lib/mp3_importer.rb"
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
+  end
+
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist
+    song
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+
+end
